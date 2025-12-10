6496f2f6e90f0d2ee6a03318928900f3f91ebfd8
Saif Kenani <saifkhaledkenani@gmail.com>
2025-12-10 13:40:25 +0300

    Add basic ANTLR parser test in Main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,33 @@
+import gen.MiniLangLexer;
+import gen.MiniLangParser;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello to flask-compiler-python");
+        // لو ما أعطيت ملف، نستخدم مثال بسيط
+        String source = """
+                x = 10;
+                y = x + 5 * 2;
+                print y;
+                print x + y;
+                """;
 
+        CharStream input = CharStreams.fromString(source);
+        // لو تبغى من ملف:
+        // CharStream input = CharStreams.fromFileName("test.mini");
+
+        MiniLangLexer lexer = new MiniLangLexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        MiniLangParser parser = new MiniLangParser(tokens);
+
+        ParseTree tree = parser.program(); // نبدأ من rule program
+
+        System.out.println(tree.toStringTree(parser));
     }
 }
