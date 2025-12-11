2ea68e160c7b9f0c8d47601458207ff849b8d626
Saif Kenani <saifkhaledkenani@gmail.com>
2025-12-11 12:41:41 +0300

    make base architecture

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import gen.MiniLangLexer;
-import gen.MiniLangParser;
+
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
@@ -10,24 +9,5 @@ import org.antlr.v4.runtime.tree.ParseTree;
 public class Main {
     public static void main(String[] args) {
 
-        // لو ما أعطيت ملف، نستخدم مثال بسيط
-        String source = """
-                x = 10;
-                y = x + 5 * 2;
-                print y;
-                print x + y;
-                """;
-
-        CharStream input = CharStreams.fromString(source);
-        // لو تبغى من ملف:
-        // CharStream input = CharStreams.fromFileName("test.mini");
-
-        MiniLangLexer lexer = new MiniLangLexer(input);
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        MiniLangParser parser = new MiniLangParser(tokens);
-
-        ParseTree tree = parser.program(); // نبدأ من rule program
-
-        System.out.println(tree.toStringTree(parser));
     }
 }
