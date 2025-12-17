284a65495dc913ec4d02cc4e5133315563b568ab
Saif Kenani <saifkhaledkenani@gmail.com>
2025-12-17 22:54:43 +0300

    Adding main to check the lexer

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,51 @@
-
+import gen.FlaskTemplateLexer;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
-import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.Token;
+import java.io.IOException;
+import org.antlr.v4.runtime.Lexer;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// ... (تأكد من استيراد PythonBasic إذا كان في حزمة مختلفة)
+// import com.yourpackage.PythonBasic;
+
 public class Main {
     public static void main(String[] args) {
 
+
+        String filePath = "test/test100";
+
+        try {
+            CharStream input = CharStreams.fromFileName(filePath);
+
+            FlaskTemplateLexer lexer = new FlaskTemplateLexer(input);
+            System.out.println("--- Starting Lexer Test: Tokens for " + filePath + " ---");
+
+            Token token;
+
+            do {
+                token = lexer.nextToken();
+
+                if (token.getChannel() != Lexer.HIDDEN) {
+                    // استخدام VOCABULARY للحصول على اسم الرمز
+                    String tokenName = FlaskTemplateLexer.VOCABULARY.getSymbolicName(token.getType());
+
+                    // تنسيق الطباعة
+                    System.out.printf("Line %-3d Col %-2d -> Type: %-10s Value: \"%s\"\n",
+                            token.getLine(),
+                            token.getCharPositionInLine(),
+                            tokenName != null ? tokenName : String.valueOf(token.getType()),
+                            // نستبدل \n بـ \\n لتكون القراءة أسهل
+                            token.getText().replace("\n", "\\n"));
+                }
+            } while (token.getType() != Token.EOF);
+
+            System.out.println("--- Lexer Test Finished ---");
+
+        } catch (IOException e) {
+            System.err.println("Error reading file: " + e.getMessage());
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
