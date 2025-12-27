219fce2892ef0b81a4c36d095fa07f4c792ecd2e
saifkenani-SW <saifkhaledkenani@gmail.com>
2025-12-28 02:47:22 +0300

    add src/ParserTest.java to test indent in python parser

--- src/ParserTest.java
+++ src/ParserTest.java
@@ -3,47 +3,51 @@ import gen.FlaskPythonParser;
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
 
+import java.io.IOException;
+
 public class ParserTest {
 
     public static void main(String[] args) throws Exception {
 
-        String code = """
-        def initialize_products_data():
-            global products_data, next_id
-            products_data.clear()
-        x+=x
-        """;
+        String filePath = "test/testJinja2";
+        String filePath2 = "test/python";
 
-        // 1. Input
-        CharStream input = CharStreams.fromString(code);
+        try {
+            CharStream input = CharStreams.fromFileName(filePath2);
 
-        // 2. Lexer
-        FlaskLexer lexer = new FlaskLexer(input);
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
+            // 2. Lexer
+            FlaskLexer lexer = new FlaskLexer(input);
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
 
-        // (اختياري) طباعة التوكنز
-        tokens.fill();
-        for (Token t : tokens.getTokens()) {
-            System.out.printf(
-                    "Line %-3d Col %-3d -> %-15s \"%s\"%n",
-                    t.getLine(),
-                    t.getCharPositionInLine(),
-                    FlaskLexer.VOCABULARY.getSymbolicName(t.getType()),
-                    t.getText()
-            );
+            // (اختياري) طباعة التوكنز
+            tokens.fill();
+            for (Token t : tokens.getTokens()) {
+                System.out.printf(
+                        "Line %-3d Col %-3d -> %-15s \"%s\"%n",
+                        t.getLine(),
+                        t.getCharPositionInLine(),
+                        FlaskLexer.VOCABULARY.getSymbolicName(t.getType()),
+                        t.getText()
+                );
+            }
+
+            // 3. Parser
+            FlaskPythonParser parser = new FlaskPythonParser(tokens);
+
+            // 4. Error handling (مهم)
+            parser.removeErrorListeners();
+            parser.addErrorListener(new DiagnosticErrorListener());
+
+            // 5. Parse (غير اسم rule حسب grammar)
+            ParseTree tree = parser.program();
+
+            // 6. طباعة الشجرة
+            System.out.println(tree.toStringTree(parser));
+        } catch (IOException e) {
+            System.err.println("Error reading file: " + e.getMessage());
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
-        // 3. Parser
-        FlaskPythonParser parser = new FlaskPythonParser(tokens);
-
-        // 4. Error handling (مهم)
-        parser.removeErrorListeners();
-        parser.addErrorListener(new DiagnosticErrorListener());
-
-        // 5. Parse (غير اسم rule حسب grammar)
-        ParseTree tree = parser.program();
-
-        // 6. طباعة الشجرة
-        System.out.println(tree.toStringTree(parser));
     }
 }
