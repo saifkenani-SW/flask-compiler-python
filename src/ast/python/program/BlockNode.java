0615496e5a518bbf78dade6e23138fb94f2c095c
saifkenani-SW <saifkhaledkenani@gmail.com>
2025-12-28 22:05:00 +0300

    Add BlockNode Python AST node

---
+++ src/ast/python/program/BlockNode.java
@@ -0,0 +1,36 @@
+package ast.python.program;
+
+import ast.python.PythonNode;
+import ast.ASTVisitor;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * كتلة من العبارات (مثل جسم دالة أو if)
+ */
+public class BlockNode extends PythonNode {
+    private List<PythonNode> statements;
+
+    public BlockNode(int line, int column) {
+        super(line, column);
+        this.statements = new ArrayList<>();
+    }
+
+    public List<PythonNode> getStatements() { return statements; }
+    public void addStatement(PythonNode statement) {
+        statements.add(statement);
+        addChild(statement);
+    }
+    public void addStatements(List<PythonNode> statements) {
+        for (PythonNode stmt : statements) {
+            addStatement(stmt);
+        }
+    }
+
+    public boolean isEmpty() { return statements.isEmpty(); }
+
+    @Override
+    public <T> T accept(ASTVisitor<T> visitor) {
+        return visitor.visit(this);
+    }
+}
