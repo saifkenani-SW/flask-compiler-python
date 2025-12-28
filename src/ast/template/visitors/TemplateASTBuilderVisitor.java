82772d3b32501952057bd247106fffefadacdf66
saifkenani-SW <saifkhaledkenani@gmail.com>
2025-12-29 00:07:46 +0300

    Adding visitTemplateRoot

--- src/ast/template/visitors/TemplateASTBuilderVisitor.java
+++ src/ast/template/visitors/TemplateASTBuilderVisitor.java
@@ -8,5 +8,23 @@ import gen.FlaskTemplateParserBaseVisitor;
 
 public class TemplateASTBuilderVisitor extends FlaskTemplateParserBaseVisitor<TemplateNode> {
 
+    @Override
+    public TemplateNode visitTemplateRoot(
+            FlaskTemplateParser.TemplateRootContext ctx) {
+
+        TemplateRootNode root =
+                new TemplateRootNode(
+                        ctx.start.getLine(),
+                        ctx.start.getCharPositionInLine()
+                );
+
+        for (var htmlCtx : ctx.html()) {
+            TemplateNode htmlNode = visit(htmlCtx);
+            root.addDocument(htmlNode);
+        }
+
+        return root;
+    }
+
 
 }
