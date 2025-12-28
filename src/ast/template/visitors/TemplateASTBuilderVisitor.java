041c95861f67f186484b0f774d5afec8bb682358
saifkenani-SW <saifkhaledkenani@gmail.com>
2025-12-29 00:07:13 +0300

    Adding TemplateASTBuilderVisitor.java

---
+++ src/ast/template/visitors/TemplateASTBuilderVisitor.java
@@ -0,0 +1,12 @@
+package ast.template.visitors;
+
+import ast.template.TemplateNode;
+import ast.template.TemplateRootNode;
+import gen.FlaskTemplateParser;
+import gen.FlaskTemplateParserBaseVisitor;
+
+
+public class TemplateASTBuilderVisitor extends FlaskTemplateParserBaseVisitor<TemplateNode> {
+
+
+}
