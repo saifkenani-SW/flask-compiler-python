1c9563202f73afcc0b804d999a8a5ce592aab3ca
saifkenani-SW <saifkhaledkenani@gmail.com>
2025-12-28 22:03:20 +0300

    Adding PythonNode.java Class

---
+++ src/ast/python/PythonNode.java
@@ -0,0 +1,34 @@
+package ast.python;
+
+import ast.ASTNode;
+import ast.ASTVisitor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class PythonNode extends ASTNode {
+
+    private final List<PythonNode> children = new ArrayList<>();
+    private PythonNode parent;
+
+    protected PythonNode(int line, int column) {
+        super(line, column);
+    }
+
+    public List<PythonNode> getChildren() {
+        return children;
+    }
+
+    public void addChild(PythonNode child) {
+        if (child != null) {
+            child.parent = this;
+            children.add(child);
+        }
+    }
+
+    public PythonNode getParent() {
+        return parent;
+    }
+
+    public abstract <T> T accept(ASTVisitor<T> visitor);
+}
