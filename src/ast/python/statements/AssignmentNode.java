e095c17a54346879a615441031828dcfabcf9bc3
saifkenani-SW <saifkhaledkenani@gmail.com>
2025-12-28 22:05:00 +0300

    Add AssignmentNode Python AST node

---
+++ src/ast/python/statements/AssignmentNode.java
@@ -0,0 +1,56 @@
+package ast.python.statements;
+
+import ast.python.PythonNode;
+import ast.ASTVisitor;
+import ast.python.expressions.ExpressionNode;
+
+/**
+ * جملة التخصيص
+ */
+public class AssignmentNode extends StatementNode {
+    private PythonNode target;
+    private String operator; // =, +=, -=, *=, /=, %=, **=, //=
+    private ExpressionNode value;
+
+    public AssignmentNode(int line, int column, PythonNode target, ExpressionNode value) {
+        super(line, column);
+        this.target = target;
+        this.operator = "=";
+        this.value = value;
+        addChild(target);
+        addChild(value);
+    }
+
+    public AssignmentNode(int line, int column, PythonNode target, String operator, ExpressionNode value) {
+        super(line, column);
+        this.target = target;
+        this.operator = operator;
+        this.value = value;
+        addChild(target);
+        addChild(value);
+    }
+
+    public PythonNode getTarget() { return target; }
+    public void setTarget(PythonNode target) {
+        this.target = target;
+        addChild(target);
+    }
+
+    public String getOperator() { return operator; }
+    public void setOperator(String operator) { this.operator = operator; }
+
+    public ExpressionNode getValue() { return value; }
+    public void setValue(ExpressionNode value) {
+        this.value = value;
+        addChild(value);
+    }
+
+    public boolean isAugmented() {
+        return !operator.equals("=");
+    }
+
+    @Override
+    public <T> T accept(ASTVisitor<T> visitor) {
+        return visitor.visit(this);
+    }
+}
