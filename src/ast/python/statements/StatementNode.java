cd723c65202483f49373286e3674aaeccb62763a
saifkenani-SW <saifkhaledkenani@gmail.com>
2026-01-03 02:23:08 +0300

    Editing StatementNode.java

--- src/ast/python/statements/StatementNode.java
+++ src/ast/python/statements/StatementNode.java
@@ -2,9 +2,6 @@ package ast.python.statements;
 
 import ast.python.PythonNode;
 
-/**
- * فئة أساسية لجميع العبارات
- */
 public abstract class StatementNode extends PythonNode {
     public StatementNode(int line, int column) {
         super(line, column);
