edba8fdb98c07c07b91d9b8022bc83fac1b7b60d
saifkenani-SW <saifkhaledkenani@gmail.com>
2026-01-03 02:22:35 +0300

    Editing BreakNode.java

--- src/ast/python/statements/BreakNode.java
+++ src/ast/python/statements/BreakNode.java
@@ -2,9 +2,6 @@ package ast.python.statements;
 
 import ast.python.visitors.PythonASTVisitor;
 
-/**
- * جملة break
- */
 public class BreakNode extends StatementNode {
 
     public BreakNode(int line, int column) {
