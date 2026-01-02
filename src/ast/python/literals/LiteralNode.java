bf089c7f5d648463f51b758539a461b5c5d2fc9f
saifkenani-SW <saifkhaledkenani@gmail.com>
2026-01-03 02:22:35 +0300

    Editing LiteralNode.java

--- src/ast/python/literals/LiteralNode.java
+++ src/ast/python/literals/LiteralNode.java
@@ -2,9 +2,6 @@ package ast.python.literals;
 
 import ast.python.expressions.ExpressionNode;
 
-/**
- * فئة أساسية لجميع القيم الحرفية
- */
 public abstract class LiteralNode extends ExpressionNode {
     public LiteralNode(int line, int column) {
         super(line, column);
