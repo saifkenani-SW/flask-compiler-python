f8af9c04d9b0f6b2b40ad287f0d2053e8c558b0b
saifkenani-SW <saifkhaledkenani@gmail.com>
2026-01-03 02:22:35 +0300

    Editing ExpressionNode.java

--- src/ast/python/expressions/ExpressionNode.java
+++ src/ast/python/expressions/ExpressionNode.java
@@ -2,9 +2,6 @@ package ast.python.expressions;
 
 import ast.python.PythonNode;
 
-/**
- * فئة أساسية لجميع التعابير
- */
 public abstract class ExpressionNode extends PythonNode {
     public ExpressionNode(int line, int column) {
         super(line, column);
